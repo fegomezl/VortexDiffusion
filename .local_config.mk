64e88f281e1e94e50f6a89cff5554135a9557738
fegomezl <fegomezl@unal.edu.co>
2022-08-01 11:23:32 -0500

    a

--- .local_config.mk
+++ .local_config.mk
@@ -1,5 +1,5 @@
 #For Spack instalations
-#MFEM_INSTALL_DIR = /home/tapemess/Repositories/spack/opt/spack/linux-arch-sandybridge/gcc-12.1.0/mfem-develop-dc5wght7ajfaqp5se2zuqnhh6i7qob4
+#MFEM_INSTALL_DIR = 
 
 #For source instalations
 #MFEM_INSTALL_DIR = /opt/MFEM/mfem/build
