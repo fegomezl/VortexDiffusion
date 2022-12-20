bec6f83d5e2a1c0c5f0be842cf0a4fdf41ee99a6
Felipe <fegomezl@unal.edu.co>
2022-12-20 01:57:56 -0500

    a

--- local_config.mk
+++ local_config.mk
@@ -1,5 +1,5 @@
 #For Spack instalations
-MFEM_INSTALL_DIR = /home/tapemess/Repositories/spack/opt/spack/linux-arch-sandybridge/gcc-12.1.0/mfem-develop-dc5wght7ajfaqp5se2zuqnhh6i7qob4u
+MFEM_INSTALL_DIR = /home/boltzmann/.spack/opt/spack/linux-arch-westmere/gcc-12.2.0/mfem-develop-ot7cjjc7fwf373bsembbdjayqejcrgsd
 
 #For source instalations
 #MFEM_INSTALL_DIR = /opt/MFEM/mfem/build
@@ -8,7 +8,7 @@ MFEM_INSTALL_DIR = /home/tapemess/Repositories/spack/opt/spack/linux-arch-sandyb
 
 #Local varibles
 PROCCESORS = 4
-SHARE_DIR = /media/sf_Arch_data/
+SHARE_DIR = /media/sf_ArchData/
 
 #Add variables from MFEM
 CONFIG_MK = $(MFEM_INSTALL_DIR)/share/mfem/config.mk
